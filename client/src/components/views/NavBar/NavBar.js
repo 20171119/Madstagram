a14d616041fe6c1893ad240bda9f1926ed5e6968
JYBESiU <jychoi.besiu@gmail.com>
2021-07-16 13:44:13 +0900

    first

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
-import { Drawer, Button, Icon } from 'antd';
+import { Drawer, Button } from 'antd';
+import Icon from '@ant-design/icons';
+// import Drawer from '@ant-design/drawers';
+// import Button from '@ant-design/buttons';
 import './Sections/Navbar.css';
 
 function NavBar() {
