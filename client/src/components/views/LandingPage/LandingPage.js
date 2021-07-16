91d0a7f4c7d2e9c5d1610f77d5aa2e7f1a4aff42
uni_20171119 <jspark98@unist.ac.kr>
2021-07-16 20:32:21 +0900

    success landing

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
 import { Col, Card, Row } from 'antd';
 import Icon from '@ant-design/icons';
+import ImageSlider from '../../utils/ImageSlider';
 
 const { Meta } = Card;
 
 function LandingPage() {
 
-    // const [Products, setProducts] = useState([])
+    const [Posts, setPosts] = useState([])
     // const [Skip, setSkip] = useState(0)
     // const [Limit, setLimit] = useState(8)
     // const [PostSize, setPostSize] = useState()
@@ -23,11 +24,29 @@ function LandingPage() {
             .then(response => {
                 if (response.data.success) {
                     console.log(response.data)
+                    setPosts(response.data.posts)
+                    console.log(Posts.length);
                 } else {
                     alert('Failed to fectch product datas')
                 }
             })
     }
+
+    const renderCards = Posts.map((post, index) => {
+
+        return <Col lg={6} md={8} xs={24}>
+            <Card
+                key={index}
+                hoverable={true}
+                cover={<a href={`/posts/${post._id}`} > <ImageSlider images={post.images} /></a>}
+            >
+                <Meta
+                    title={post.title}
+                    content={`${post.content}`}
+                />
+            </Card>
+        </Col>
+    })
 
 
     return (
@@ -35,6 +54,18 @@ function LandingPage() {
             <div style={{ textAlign: 'center' }}>
                 <h2>  Let's Travel Anywhere  <Icon type="rocket" />  </h2>
             </div>
+
+            {Posts.length === 0 ?
+                <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+                    <h2>No post yet...</h2>
+                </div> :
+                <div>
+                    <Row gutter={[16, 16]}>
+    
+                        {renderCards}
+                    </Row>
+                </div>
+            }
         </div>
     )
 }
