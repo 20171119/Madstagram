bf97ecb39316d01ff092fa87908d4a1740438951
JYBESiU <joyo10@kaist.ac.kr>
2021-07-16 15:41:33 +0000

    first

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'http://172.10.18.171:80',
             changeOrigin: true,
         })
     );
